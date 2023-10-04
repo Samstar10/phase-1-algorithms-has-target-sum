462d8da8d57e4614902bb4fe700a68e07ee51565
Samuel Muli <samuelmuli95@gmail.com>
2023-10-04 14:40:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(let i = 0; i < array.length; i++){
+    const remainder = target - array[i]
+    for(let j = i + 1; j < array.length; j++){
+      if(array[j] === remainder){
+        return true
+      }
+    }
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
 */
+//0(n*n)
 
 /* 
   Add your pseudocode here
 */
+//writing a loop to iterate over the array for the first time
+//Nest another loop inside the first loop
+//Write an if statement to compare the two values to confirm if they add up to the target.
 
 /*
   Add written explanation of your solution here
 */
+// So from what I understand is that I am given an array and a target number. I should iterate through the array to get two numbers that add up to the target and if they do exist I should return true or false if the combination does not exist.
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
